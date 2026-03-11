172dfb2512d0db7be4ab8d6a73e36cefdd6bf236
ustbxcj <2248190184@qq.com>
2026-03-11 22:11:04 +0800

    腿部还是得张开

--- source/MyProject/MyProject/tasks/manager_based/WalkTest/walk_bishe_env_cfg.py
+++ source/MyProject/MyProject/tasks/manager_based/WalkTest/walk_bishe_env_cfg.py
@@ -378,7 +378,7 @@ class BiShePitRewardsCfg(RewardsCfg):
         func=walk_mdp.feet_too_near,
         weight=-1.0,
         params={
-            "threshold": 0.15,
+            "threshold": 0.20,
             "asset_cfg": SceneEntityCfg("robot", body_names=["RL_foot", "RR_foot"]),
         },
     )
